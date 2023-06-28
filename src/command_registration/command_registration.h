6c3a807428cdcf33ad45605aad4efe8d54eaf0d3
Dexter <dexterwest317@gmail.com>
2023-06-27 18:41:24 -0700

    Added a compile time check for commands declared with required positionals after optional ones

--- src/command_registration/command_registration.h
+++ src/command_registration/command_registration.h
@@ -167,11 +167,23 @@ namespace cppli::detail {
     }
 
     // TODO: static_assert for optional positions followed by required positionals
-    template<typename arg_t, typename...arg_ts>
+    template<bool optional_positional_enountered, typename arg_t, typename...arg_ts>
     constexpr std::size_t count_positionals() {
-        constexpr bool is_positional = argument_info_t<std::remove_cvref_t<arg_t>>::is_positional;
+        using T = std::remove_cvref_t<arg_t>;
+        using arg_info = argument_info_t<T>;
+
+        constexpr bool is_positional = arg_info::is_positional;
+        if constexpr(is_positional && optional_positional_enountered) {
+            static_assert(T::optional, "required positionals cannot follow optional positionals");
+        }
+
         if constexpr(sizeof...(arg_ts) > 0) {
-            return count_positionals<arg_ts...>()+is_positional;
+            if constexpr(is_positional) {
+                return count_positionals<optional_positional_enountered || T::optional, arg_ts...>()+is_positional;
+            }
+            else {
+                return count_positionals<optional_positional_enountered, arg_ts...>()+is_positional;
+            }
         }
         else {
             return is_positional;
@@ -186,189 +198,188 @@ namespace cppli::detail {
 
     template<typename return_t, typename...arg_ts, std::size_t...indices, auto func>
     struct call_func_wrapper_impl_t<return_t(*)(arg_ts...), func, std::integer_sequence<std::size_t, indices...>> {
-    static void call_func(const subcommand_t& subcommand) {
-        func(process_argument<std::remove_cvref_t<get_type_from_index_in_pack<indices, arg_ts...>>>(count_positionals_before_index_v<indices, arg_ts...>, subcommand)...);
-    }
-};
-
-template<typename func_t, auto func>
-struct call_func_wrapper_t;
-
-template<typename return_t, typename...arg_ts, auto func>
-struct call_func_wrapper_t<return_t(*)(arg_ts...), func> { // we need all this ugly partial specializations so that we can deduce arg_ts and indices
-    static void call_func(const subcommand_t& subcommand) {
-        constexpr auto positionals_count = count_positionals<arg_ts...>();
-        if(positionals_count < subcommand.inputs.positional_args.size()) {
-            std::cerr << "Too many positional arguments given to " << (subcommand.name == subcommand_name_t{"MAIN"} ? "main command" : "subcommand") << " \"" << subcommand_name_to_docs()[subcommand.name].name <<
-                      "\" (expected " << std::to_string(positionals_count) << ", got " << std::to_string(subcommand.inputs.positional_args.size()) << "). Excess positional argument will be ignored\n";
+        static void call_func(const subcommand_t& subcommand) {
+            func(process_argument<std::remove_cvref_t<get_type_from_index_in_pack<indices, arg_ts...>>>(count_positionals_before_index_v<indices, arg_ts...>, subcommand)...);
         }
-        call_func_wrapper_impl_t<decltype(func), func, std::make_index_sequence<sizeof...(arg_ts)>>::call_func(subcommand);
-    }
-};
+    };
 
-template<auto func>
-struct call_func_wrapper_t<void(*)(), func> { // we need all this ugly partial specializations so that we can deduce arg_ts and indices
-    static void call_func(const subcommand_t& subcommand) {
-        func();
-    }
-};
+    template<typename func_t, auto func>
+    struct call_func_wrapper_t;
 
-template<auto func>
-void call_func(const subcommand_t& command) {
-    call_func_wrapper_t<decltype(func), func>::call_func(command);
-}
-
-template<typename T, typename U, typename...Ts>
-constexpr bool pack_contains_short_name_func() {
-    if constexpr(T::has_short_name && U::has_short_name) {
-        if constexpr(sizeof...(Ts) > 0) {
-            return (T::short_name == U::short_name) || pack_contains_short_name_func<T, Ts...>();
+    template<typename return_t, typename...arg_ts, auto func>
+    struct call_func_wrapper_t<return_t(*)(arg_ts...), func> { // we need all this ugly partial specializations so that we can deduce arg_ts and indices
+        static void call_func(const subcommand_t& subcommand) {
+            constexpr auto positionals_count = count_positionals<false, arg_ts...>();
+            if(positionals_count < subcommand.inputs.positional_args.size()) {
+                std::cerr << "Too many positional arguments given to " << (subcommand.name == subcommand_name_t{"MAIN"} ? "main command" : "subcommand") << " \"" << subcommand_name_to_docs()[subcommand.name].name <<
+                          "\" (expected " << std::to_string(positionals_count) << ", got " << std::to_string(subcommand.inputs.positional_args.size()) << "). Excess positional argument will be ignored\n";
+            }
+            call_func_wrapper_impl_t<decltype(func), func, std::make_index_sequence<sizeof...(arg_ts)>>::call_func(subcommand);
         }
-        else {
-            return (T::short_name == U::short_name);
+    };
+
+    template<auto func>
+    struct call_func_wrapper_t<void(*)(), func> { // we need all this ugly partial specializations so that we can deduce arg_ts and indices
+        static void call_func(const subcommand_t& subcommand) {
+            func();
         }
-    }
-    else {
-        return false;
-    }
-}
+    };
 
-template<std::size_t index, typename first_t, typename...rest_ts>
-constexpr bool no_repeated_short_names_func() {
-    if constexpr(sizeof...(rest_ts) == 0) {
-        return true;
+    template<auto func>
+    void call_func(const subcommand_t& command) {
+        call_func_wrapper_t<decltype(func), func>::call_func(command);
     }
-    else if constexpr(index < sizeof...(rest_ts)) {
-        return (!pack_contains_short_name_func<first_t, rest_ts...>()) && no_repeated_short_names_func<index+1, rest_ts..., first_t>();
-    }
-    else {
-        return (!pack_contains_short_name_func<first_t, rest_ts...>());
-    }
-}
 
-template<typename...arg_ts>
-constexpr bool no_repeated_short_names_v = no_repeated_short_names_func<0, std::remove_cvref_t<arg_ts>...>();
-
-
-template<typename T, typename U, typename...Ts>
-constexpr bool pack_contains_long_name_func() {
-    if constexpr(T::has_long_name && U::has_long_name) {
-        if constexpr(sizeof...(Ts) > 0) {
-            return (T::name == U::name) || pack_contains_short_name_func<T, Ts...>();
+    template<typename T, typename U, typename...Ts>
+    constexpr bool pack_contains_short_name_func() {
+        if constexpr(T::has_short_name && U::has_short_name) {
+            if constexpr(sizeof...(Ts) > 0) {
+                return (T::short_name == U::short_name) || pack_contains_short_name_func<T, Ts...>();
+            }
+            else {
+                return (T::short_name == U::short_name);
+            }
         }
         else {
-            return (T::name == U::name);
+            return false;
         }
     }
-    else {
-        return false;
-    }
-}
 
-template<std::size_t index, typename first_t, typename...rest_ts>
-constexpr bool no_repeated_long_names_func() {
-    if constexpr(sizeof...(rest_ts) == 0) {
-        return true;
-    }
-    else if constexpr(index < sizeof...(rest_ts)) {
-        return (!pack_contains_long_name_func<first_t, rest_ts...>()) && no_repeated_long_names_func<index+1, rest_ts..., first_t>();
-    }
-    else {
-        return (!pack_contains_long_name_func<first_t, rest_ts...>());
-    }
-}
-
-template<typename...arg_ts>
-constexpr bool no_repeated_long_names_v = no_repeated_long_names_func<0, std::remove_cvref_t<arg_ts>...>();
-
-
-template<typename return_t, typename arg_t, typename...arg_ts>                      // don't actually care about func, just need to deduce args
-void generate_input_info_and_docs(subcommand_inputs_info_t& info, subcommand_documentation_t& documentation, return_t(*func)(arg_t, arg_ts...) = nullptr) {
-    using arg_info_t = argument_info_t<std::remove_cvref_t<arg_t>>;
-    using type = std::remove_cvref_t<arg_t>;
-
-    static_assert(no_repeated_short_names_v<arg_t, arg_ts...>, "multiple flags/options cannot share a short name");
-    static_assert(no_repeated_long_names_v<arg_t, arg_ts...>,  "multiple flags/options cannot share a long name");
-
-    if constexpr(arg_info_t::is_flag) {
-        documentation.flags.emplace(type::name.string(),
-                                    type::documentation.string(),
-                                    type::short_name);
-
-        info.flags.insert(type::name.string());
-
-        if(type::short_name) {
-            info.flags.insert(std::string{type::short_name});
+    template<std::size_t index, typename first_t, typename...rest_ts>
+    constexpr bool no_repeated_short_names_func() {
+        if constexpr(sizeof...(rest_ts) == 0) {
+            return true;
+        }
+        else if constexpr(index < sizeof...(rest_ts)) {
+            return (!pack_contains_short_name_func<first_t, rest_ts...>()) && no_repeated_short_names_func<index+1, rest_ts..., first_t>();
+        }
+        else {
+            return (!pack_contains_short_name_func<first_t, rest_ts...>());
         }
     }
-    else if constexpr(arg_info_t::is_option) {
-        documentation.options.emplace(type::type_string.string(),
-                                      type::name.string(),
-                                      type::argument_text.string(),
-                                      type::documentation.string(),
-                                      type::short_name,
 
-                                      type::optional,
-                                      type::argument_optional);
+    template<typename...arg_ts>
+    constexpr bool no_repeated_short_names_v = no_repeated_short_names_func<0, std::remove_cvref_t<arg_ts>...>();
 
-        info.option_argument_is_optional.emplace(type::name.string(), type::argument_optional);
 
-        if constexpr(type::short_name != '\0') {
-            info.option_argument_is_optional.emplace(std::string{type::short_name}, type::argument_optional);
+    template<typename T, typename U, typename...Ts>
+    constexpr bool pack_contains_long_name_func() {
+        if constexpr(T::has_long_name && U::has_long_name) {
+            if constexpr(sizeof...(Ts) > 0) {
+                return (T::name == U::name) || pack_contains_short_name_func<T, Ts...>();
+            }
+            else {
+                return (T::name == U::name);
+            }
+        }
+        else {
+            return false;
         }
     }
-    else { // positional
-        documentation.positionals.emplace_back(type::type_string.string(),
-                                               type::name.string(),
-                                               type::documentation.string(),
 
-                                               type::optional);
-    }
-
-    if constexpr(sizeof...(arg_ts) > 0) {
-        generate_input_info_and_docs<return_t, arg_ts...>(info, documentation);
-    }
-}
-
-inline void generate_input_info_and_docs(subcommand_inputs_info_t& info, subcommand_documentation_t& documentation, void(*func)() = nullptr) {
-    // do nothing
-}
-
-template<auto func>
-dummy_t register_subcommand(const subcommand_name_t& name, const char* description) {
-    subcommand_inputs_info_t   info;
-    subcommand_documentation_t docs(name.back(), description);
-
-    subcommand_name_t cumulative_name;
-    for(unsigned i = 0; i < name.size()-1; ++i) {
-        cumulative_name.push_back(name[i]);
-        if(!subcommand_name_to_docs().contains(cumulative_name)) {
-            subcommand_name_to_docs()[cumulative_name].name = cumulative_name.back();
+    template<std::size_t index, typename first_t, typename...rest_ts>
+    constexpr bool no_repeated_long_names_func() {
+        if constexpr(sizeof...(rest_ts) == 0) {
+            return true;
         }
-        subcommand_name_to_docs()[cumulative_name].subcommands.emplace(name[i+1]);
+        else if constexpr(index < sizeof...(rest_ts)) {
+            return (!pack_contains_long_name_func<first_t, rest_ts...>()) && no_repeated_long_names_func<index+1, rest_ts..., first_t>();
+        }
+        else {
+            return (!pack_contains_long_name_func<first_t, rest_ts...>());
+        }
     }
 
-    /*if(name.front() != "MAIN") {
-        subcommand_name_to_docs()[{"MAIN"}].subcommands.emplace(name.front());
-    }*/
+    template<typename...arg_ts>
+    constexpr bool no_repeated_long_names_v = no_repeated_long_names_func<0, std::remove_cvref_t<arg_ts>...>();
 
-    generate_input_info_and_docs(info, docs, func);
+    template<typename return_t, typename arg_t, typename...arg_ts>                      // don't actually care about func, just need to deduce args
+    void generate_input_info_and_docs(subcommand_inputs_info_t& info, subcommand_documentation_t& documentation, return_t(*func)(arg_t, arg_ts...) = nullptr) {
+        using arg_info_t = argument_info_t<std::remove_cvref_t<arg_t>>;
+        using type = std::remove_cvref_t<arg_t>;
 
-    subcommand_name_to_func().emplace(name, call_func<func>);
-    subcommand_name_to_inputs_info().emplace(name, std::move(info));
+        static_assert(no_repeated_short_names_v<arg_t, arg_ts...>, "multiple flags/options cannot share a short name");
+        static_assert(no_repeated_long_names_v<arg_t, arg_ts...>,  "multiple flags/options cannot share a long name");
 
-    if(name == subcommand_name_t{"MAIN"}) {
-        subcommand_name_to_docs()[name].flags = std::move(docs.flags);
-        subcommand_name_to_docs()[name].options = std::move(docs.options);
-        subcommand_name_to_docs()[name].positionals = std::move(docs.positionals);
-        subcommand_name_to_docs()[name].is_namespace = false;
+        if constexpr(arg_info_t::is_flag) {
+            documentation.flags.emplace(type::name.string(),
+                                        type::documentation.string(),
+                                        type::short_name);
 
-        // can't do a full assignment because that would wipe subcommands
+            info.flags.insert(type::name.string());
+
+            if(type::short_name) {
+                info.flags.insert(std::string{type::short_name});
+            }
+        }
+        else if constexpr(arg_info_t::is_option) {
+            documentation.options.emplace(type::type_string.string(),
+                                          type::name.string(),
+                                          type::argument_text.string(),
+                                          type::documentation.string(),
+                                          type::short_name,
+
+                                          type::optional,
+                                          type::argument_optional);
+
+            info.option_argument_is_optional.emplace(type::name.string(), type::argument_optional);
+
+            if constexpr(type::short_name != '\0') {
+                info.option_argument_is_optional.emplace(std::string{type::short_name}, type::argument_optional);
+            }
+        }
+        else { // positional
+            documentation.positionals.emplace_back(type::type_string.string(),
+                                                   type::name.string(),
+                                                   type::documentation.string(),
+
+                                                   type::optional);
+        }
+
+        if constexpr(sizeof...(arg_ts) > 0) {
+            generate_input_info_and_docs<return_t, arg_ts...>(info, documentation);
+        }
     }
-    else {
-        subcommand_name_to_docs().insert_or_assign(name, std::move(docs));
+
+    inline void generate_input_info_and_docs(subcommand_inputs_info_t& info, subcommand_documentation_t& documentation, void(*func)() = nullptr) {
+        // do nothing
     }
 
-    return {};
-}
+    template<auto func>
+    dummy_t register_subcommand(const subcommand_name_t& name, const char* description) {
+        subcommand_inputs_info_t   info;
+        subcommand_documentation_t docs(name.back(), description);
+
+        subcommand_name_t cumulative_name;
+        for(unsigned i = 0; i < name.size()-1; ++i) {
+            cumulative_name.push_back(name[i]);
+            if(!subcommand_name_to_docs().contains(cumulative_name)) {
+                subcommand_name_to_docs()[cumulative_name].name = cumulative_name.back();
+            }
+            subcommand_name_to_docs()[cumulative_name].subcommands.emplace(name[i+1]);
+        }
+
+        /*if(name.front() != "MAIN") {
+            subcommand_name_to_docs()[{"MAIN"}].subcommands.emplace(name.front());
+        }*/
+
+        generate_input_info_and_docs(info, docs, func);
+
+        subcommand_name_to_func().emplace(name, call_func<func>);
+        subcommand_name_to_inputs_info().emplace(name, std::move(info));
+
+        if(name == subcommand_name_t{"MAIN"}) {
+            subcommand_name_to_docs()[name].flags = std::move(docs.flags);
+            subcommand_name_to_docs()[name].options = std::move(docs.options);
+            subcommand_name_to_docs()[name].positionals = std::move(docs.positionals);
+            subcommand_name_to_docs()[name].is_namespace = false;
+
+            // can't do a full assignment because that would wipe subcommands
+        }
+        else {
+            subcommand_name_to_docs().insert_or_assign(name, std::move(docs));
+        }
+
+        return {};
+    }
 }
