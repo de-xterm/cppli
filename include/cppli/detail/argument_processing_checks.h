6cfa5a4241ae41908a509196a5d6d0cc230a1344
Dexter <dexterwest317@gmail.com>
2023-08-14 17:41:36 -0700

    Moved a bunch of code out of command_registration.h and into new files

---
+++ include/cppli/detail/argument_processing_checks.h
@@ -0,0 +1,115 @@
+#pragma once
+
+namespace cppli::detail {
+    template<std::size_t current_index, std::size_t max_index, typename arg_t, typename...arg_ts>
+    constexpr std::size_t count_positionals_before_index_func() {
+        if constexpr(current_index < max_index) {
+            return count_positionals_before_index_func<current_index+1, max_index, arg_ts...>()+argument_info_t<std::remove_cvref_t<arg_t>>::is_positional;
+        }
+        else {
+            return 0; // return 0 instead of is_positional because inclusivity is not what we want
+        }
+    }
+
+    struct dummy_t{};
+
+    // TODO: static_assert for optional positions followed by required positionals
+    template<bool optional_positional_encountered, typename arg_t, typename...arg_ts>
+    constexpr bool invalid_optional_positionals_found() {
+        using T = std::remove_cvref_t<arg_t>;
+        constexpr bool is_positional = argument_info_t<T>::is_positional;
+
+        if constexpr(sizeof...(arg_ts) > 0) {
+            if constexpr(is_positional) {
+                return (optional_positional_encountered && !T::optional) || invalid_optional_positionals_found<optional_positional_encountered || T::optional, arg_ts...>();
+            }
+            else {
+                return invalid_optional_positionals_found<optional_positional_encountered, arg_ts...>();
+            }
+        }
+        else {
+            if constexpr(is_positional) {
+                return optional_positional_encountered && !T::optional;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    template<typename arg_t, typename...arg_ts>
+    constexpr std::size_t count_variadics() {
+        if constexpr(sizeof...(arg_ts) > 0) {
+            return argument_info_t<arg_t>::is_variadic+count_variadics<arg_ts...>();
+        }
+        else {
+            return 0;
+        }
+    }
+
+    template<std::size_t max_index, typename...arg_ts>
+    static constexpr std::size_t count_positionals_before_index_v = count_positionals_before_index_func<0, max_index, arg_ts...>();
+
+    template<typename T, typename U, typename...Ts>
+    constexpr bool pack_contains_short_name_func() {
+        if constexpr(argument_info_t<T>::has_short_name && argument_info_t<U>::has_short_name) {
+            if constexpr(sizeof...(Ts) > 0) {
+                return (T::short_name == U::short_name) || pack_contains_short_name_func<T, Ts...>();
+            }
+            else {
+                return (T::short_name == U::short_name);
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    template<std::size_t index, typename first_t, typename...rest_ts> // this is broken
+    constexpr bool no_repeated_short_names_func() {
+        if constexpr(sizeof...(rest_ts) == 0) {
+            return true;
+        }
+        else if constexpr(index < sizeof...(rest_ts)) {
+            return (!pack_contains_short_name_func<first_t, rest_ts...>()) && no_repeated_short_names_func<index+1, rest_ts..., first_t>();
+        }
+        else {
+            return (!pack_contains_short_name_func<first_t, rest_ts...>());
+        }
+    }
+
+    template<typename...arg_ts>
+    constexpr bool no_repeated_short_names_v = no_repeated_short_names_func<0, std::remove_cvref_t<arg_ts>...>();
+
+
+    template<typename T, typename U, typename...Ts>
+    constexpr bool pack_contains_long_name_func() {
+        if constexpr(argument_info_t<T>::has_long_name && argument_info_t<U>::has_long_name) {
+            if constexpr(sizeof...(Ts) > 0) {
+                return (T::name == U::name) || pack_contains_short_name_func<T, Ts...>();
+            }
+            else {
+                return (T::name == U::name);
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    template<std::size_t index, typename first_t, typename...rest_ts>
+    constexpr bool no_repeated_long_names_func() {
+        if constexpr(sizeof...(rest_ts) == 0) {
+            return true;
+        }
+        else if constexpr(index < sizeof...(rest_ts)) {
+            return (!pack_contains_long_name_func<first_t, rest_ts...>()) && no_repeated_long_names_func<index+1, rest_ts..., first_t>();
+        }
+        else {
+            return (!pack_contains_long_name_func<first_t, rest_ts...>());
+        }
+    }
+
+    template<typename...arg_ts>
+    constexpr bool no_repeated_long_names_v = no_repeated_long_names_func<0, std::remove_cvref_t<arg_ts>...>();
+}
---
+++ include/cppli/detail/process_argument.h
@@ -0,0 +1,354 @@
+#pragma once
+
+#include "parameter_types.h"
+
+namespace cppli::detail {
+    template<typename T, typename last, typename conversion_t>
+    T process_positional(unsigned cumulative_positional_index, const subcommand_t& subcommand,
+                         const std::string& main_command_or_subcommand, const std::string& long_name) {
+        --cumulative_positional_index; // because we've already skipped over the actual parameter for the positional, cumulative_positional_index will be too big
+
+        if constexpr(last::optional) {
+            if (cumulative_positional_index < subcommand.inputs.positional_args.size()) {
+                try {
+                    return conversion_t()(
+                            subcommand.inputs.positional_args[cumulative_positional_index]);
+                }
+                catch (user_error& e) {
+                    throw user_error("Error initializing " + main_command_or_subcommand + " \"" +
+                                     to_string(subcommand.name) + "\" positional argument \"" +
+                                     long_name + "\". Details: " + e.what(), e.error_type());
+                }
+            }
+            else {
+                return {}; // empty optional
+            }
+        }
+        else {
+            if ((cumulative_positional_index >= subcommand.inputs.positional_args.size())) {
+                throw user_error(main_command_or_subcommand + " \"" + to_string(subcommand.name) +
+                                 "\" required positional argument \"" + long_name +
+                                 "\" was not provided (expected an argument of type " +
+                                 conversion_t::type_string.string() + ')',
+                                 REQUIRED_POSITIONAL_NOT_PROVIDED);
+            }
+            else {
+                try {
+                    return conversion_t()(
+                            subcommand.inputs.positional_args[cumulative_positional_index]);
+                }
+                catch (user_error& e) {
+                    throw user_error("Error initializing " + main_command_or_subcommand + " \"" +
+                                     to_string(subcommand.name) + "\" positional argument \"" +
+                                     long_name + "\". Details: " + e.what(), e.error_type());
+                }
+            }
+        }
+    }
+
+    template<typename T, typename last, typename conversion_t>
+    T process_variadic(unsigned cumulative_positional_index, const subcommand_t& subcommand,
+                       const std::string& main_command_or_subcommand, const std::string& long_name) {
+        T ret; // T is a vector
+        for (unsigned i = cumulative_positional_index;
+             i < subcommand.inputs.positional_args.size(); ++i) {
+            try {
+                ret.emplace_back(conversion_t()(subcommand.inputs.positional_args[i]));
+            }
+            catch (user_error& e) {
+                throw user_error("Error initializing " + main_command_or_subcommand + " \"" +
+                                 to_string(subcommand.name) + "\" variadic argument pack \"" +
+                                 long_name + "\" at argument index " + std::to_string(i) +
+                                 ". Details: " + e.what(), e.error_type());
+            }
+        }
+        return ret;
+    }
+
+    template<typename T, typename last, typename conversion_t>
+    T process_required_option(const subcommand_t& subcommand,
+                                       const std::string& main_command_or_subcommand,
+                                       const std::string& long_name, const std::optional<std::string>& short_name) {
+        
+        if (!subcommand.inputs.options_to_values.contains(long_name) &&
+            (short_name && !subcommand.inputs.options_to_values.contains(*short_name))) {
+
+            throw user_error(main_command_or_subcommand + " \"" + to_string(subcommand.name) +
+                             "\" was not provided with required option \"" + long_name + '"',
+                             REQUIRED_OPTION_NOT_PROVIDED);
+        }
+        else if ((subcommand.inputs.options_to_values.contains(long_name) &&
+                  !subcommand.inputs.options_to_values.at(long_name).has_value()) ||
+                 (short_name && subcommand.inputs.options_to_values.contains(*short_name) &&
+                  !subcommand.inputs.options_to_values.at(*short_name).has_value())) {
+
+            throw user_error(
+                    main_command_or_subcommand + " \"" + to_string(subcommand.name) + "\" option \""
+                    + long_name + "\" requires an argument, but one was not provided "
+                                       "(expected an argument of type "
+                    + conversion_t::type_string.make_lowercase_and_convert_underscores().string() +
+                    ')',
+                    OPTION_REQUIRED_ARGUMENT_NOT_PROVIDED);
+        }
+        else { // by this point, none of the optionals we're interested in are empty
+            if (subcommand.inputs.options_to_values.contains(long_name)) {
+                try {
+                    return conversion_t()(*subcommand.inputs.options_to_values.at(long_name));
+                }
+                catch (user_error& e) {
+                    throw user_error("Error initializing " + main_command_or_subcommand + " \""
+                                     + to_string(subcommand.name) + "\" option \"" +
+                                     long_name + "\". Details: " + e.what(), e.error_type());
+                }
+            }
+            else { // has_short_name is guaranteed to be true at this point
+                try {
+                    return conversion_t()(*subcommand.inputs.options_to_values.at(*short_name));
+                }
+                catch (user_error& e) {
+                    throw user_error("Error initializing " + main_command_or_subcommand + " \"" +
+                                     to_string(subcommand.name) + "\" option \"" + *short_name +
+                                     "\" (full name \"" + long_name + "\"). Details: " +
+                                     e.what(), e.error_type());
+                }
+            }
+        }
+    }
+
+    template<typename T, typename last, typename conversion_t>
+    T process_required_argument_option(const subcommand_t& subcommand,
+                                       const std::string& main_command_or_subcommand,
+                                       const std::string& long_name, const std::optional<std::string>& short_name) {
+
+        if (subcommand.inputs.options_to_values.contains(long_name)) {
+            if (!subcommand.inputs.options_to_values.at(
+                    long_name).has_value()) { // TODO: aren't we doing this check in arg_parsing.cpp too?
+                throw user_error(main_command_or_subcommand + to_string(subcommand.name) +
+                                 "\" option \"" + long_name + "\" "
+                                                                   "requires an argument, but one was not provided (expected an argument of type "
+                                 +
+                                 static_cast<std::string>(conversion_t::type_string.make_lowercase_and_convert_underscores()) +
+                                 "."
+                                 "Note that this option is optional, so it is valid to omit it entirely, "
+                                 "but the option's argument is required, so if the option is provided, it must come with an argument",
+                                 OPTION_REQUIRED_ARGUMENT_NOT_PROVIDED);
+            }
+            else {
+                try {
+                    return conversion_t()(*subcommand.inputs.options_to_values.at(
+                            long_name)); // no need for has_value check here; returning an empty optional is valid
+                }
+                catch (user_error& e) {
+                    throw user_error(
+                            "Error initializing " + main_command_or_subcommand + " \"" +
+                            to_string(subcommand.name) + "\" option \"" + long_name +
+                            "\". Details: " + e.what(), e.error_type());
+                }
+            }
+        }
+        else if (short_name && subcommand.inputs.options_to_values.contains(
+                *short_name)) { // TODO: evaluating short_name could use if constexpr
+            if (!subcommand.inputs.options_to_values.at(*short_name).has_value()) {
+                throw user_error(main_command_or_subcommand + to_string(subcommand.name) +
+                                 "\" option \"" + *short_name + "\" (full name \"" +
+                                 long_name + "\") "
+                                                  "requires an argument, but one was not provided (expected an argument of type "
+                                 + conversion_t::type_string.string() + "."
+                                                                        "Note that this option is optional, so it is valid to omit it entirely, "
+                                                                        "but the option's argument is required, so if the option is provided, it must come with an argument",
+                                 OPTION_REQUIRED_ARGUMENT_NOT_PROVIDED);
+            }
+            else {
+                try {
+                    return conversion_t()(*subcommand.inputs.options_to_values.at(
+                            *short_name)); // no need for has_value check here; returning an empty optional is valid
+                }
+                catch (user_error& e) {
+                    throw user_error(
+                            "Error initializing " + main_command_or_subcommand + " \"" +
+                            to_string(subcommand.name) + "\" option \"" + *short_name +
+                            "\" (full name \"" + long_name + "\"). Details: " + e.what(),
+                            e.error_type());
+                }
+            }
+        }
+        else {
+            return {};
+        }
+    }
+
+    template<typename T, typename last, typename conversion_t>
+    T process_raw_type_optional_argument_option(const subcommand_t& subcommand,
+                                                const std::string& main_command_or_subcommand,
+                                                const std::string& long_name, const std::optional<std::string>& short_name) {
+
+        if (subcommand.inputs.options_to_values.contains(long_name)) {
+            if (subcommand.inputs.options_to_values.at(long_name).has_value()) {
+                try {
+                    return conversion_t()(*subcommand.inputs.options_to_values.at(
+                            long_name)); // no need for has_value check here; returning an empty optional is valid
+                }
+                catch (user_error& e) {
+                    throw user_error(
+                            "Error initializing " + main_command_or_subcommand + " \"" +
+                            to_string(subcommand.name) + "\" option \"" + long_name +
+                            "\". Details: " + e.what(), e.error_type());
+                }
+            }
+            else {
+                return typename last::type();
+            }
+        }
+        else if (short_name && subcommand.inputs.options_to_values.contains(*short_name)) {
+            if (subcommand.inputs.options_to_values.at(*short_name).has_value()) {
+                try {
+                    return conversion_t()(*subcommand.inputs.options_to_values.at(*short_name));
+                }
+                catch (user_error& e) {
+                    throw user_error(
+                            "Error initializing " + main_command_or_subcommand + " \"" +
+                            to_string(subcommand.name) + "\" option \"" + *short_name +
+                            "\" (full name \"" + long_name + "\"). Details: " +
+                            e.what(), e.error_type());
+                }
+            }
+            else {
+                return typename last::type();
+            }
+        }
+        else {
+            return {}; // return empty optional
+        }
+    }
+
+    template<typename T, typename last>
+    T process_optional_argument_option(const subcommand_t& subcommand,
+                                                const std::string& main_command_or_subcommand,
+                                                const std::string& long_name, const std::optional<std::string>& short_name) {
+
+        if (subcommand.inputs.options_to_values.contains(long_name)) {
+            try {
+                return {subcommand.inputs.options_to_values.at(
+                        long_name)}; // no need for has_value check here; returning an empty optional is valid
+            }
+            catch (user_error& e) {
+                throw user_error("Error initializing " + main_command_or_subcommand + " \"" +
+                                 to_string(subcommand.name) + "\" option \"" + long_name +
+                                 "\". Details: " + e.what(), e.error_type());
+            }
+        }
+        else if (short_name && subcommand.inputs.options_to_values.contains(*short_name)) {
+            try {
+                return {subcommand.inputs.options_to_values.at(
+                        *short_name)}; // no need for has_value check here; returning an empty optional is valid
+            }
+            catch (user_error& e) {
+                throw user_error("Error initializing " + main_command_or_subcommand + " \"" +
+                                 to_string(subcommand.name) + "\" option \"" + *short_name +
+                                 "\" (full name \"" + long_name + "\"). Details: " + e.what(),
+                                 e.error_type());
+            }
+        }
+        else {
+            return {}; // default constructor for optional option with optional argument inits to empty optional and was_included to false
+        }
+    }
+
+    template<typename T, typename last>
+    T process_raw_type(unsigned cumulative_positional_index, const subcommand_t& subcommand,
+                                       const std::string& main_command_or_subcommand) {
+
+        std::string long_name = last::name.string();
+
+        std::optional<std::string> short_name;
+
+        if constexpr(last::has_short_name) {
+            short_name = std::string{last::short_name};
+        }
+
+        if constexpr(argument_info_t<last>::is_flag) {
+            return subcommand.inputs.flags.contains(last::name.string()) ||
+                   (short_name && subcommand.inputs.flags.contains(std::string{last::short_name}));
+        }
+        else {
+            using conversion_t = typename last::conversion_t;
+
+            if constexpr(argument_info_t<last>::is_option) {
+                if constexpr(last::optional) { // implicitly required argument
+                    if constexpr(last::argument_optional) { // I believe default_construct_when_empty is implicity true, so no need for && here
+                        return process_raw_type_optional_argument_option<T, last, conversion_t>(subcommand, main_command_or_subcommand,
+                                                                                  long_name, short_name);
+                    }
+                    else {
+                        return process_required_argument_option<T, last, conversion_t>(subcommand, main_command_or_subcommand, 
+                                                                                       long_name, short_name);
+                    }
+                }
+                else {
+                    return process_required_option<T, last, conversion_t>(subcommand,
+                                                                          main_command_or_subcommand,
+                                                                          long_name, short_name);
+                }
+            }
+            else if constexpr(argument_info_t<last>::is_positional) { // positional
+                return process_positional<T, last, conversion_t>(cumulative_positional_index, subcommand,
+                                                                 main_command_or_subcommand, long_name);
+            }
+            else { // variadic
+                return process_variadic<T, last, conversion_t>(cumulative_positional_index, subcommand,
+                                                               main_command_or_subcommand, long_name);
+            }
+        }
+    }
+
+    template<typename T, typename last>
+    T process_wrapper(const subcommand_t& subcommand,
+                      const std::string& main_command_or_subcommand) {
+
+
+        std::string long_name = T::name.string();
+
+        std::optional<std::string> short_name;
+
+        if constexpr(T::has_short_name) {
+            short_name = std::string{T::short_name};
+        }
+
+        if constexpr(argument_info_t<T>::is_option) {
+            if constexpr(T::optional && T::argument_optional) {
+                return process_optional_argument_option<T, last>(subcommand,
+                                                                 main_command_or_subcommand,
+                                                                 long_name, short_name);
+            }
+            else {
+                return {}; // this variable is a dummy
+            }
+        }
+        else {
+            return {}; // dummy
+        }
+    }
+
+    template<typename T, typename last>
+    /*add_const_ref_if_string_t<T>::*/T
+    process_argument(unsigned cumulative_positional_index, const subcommand_t& subcommand) {
+        using arg_info_t = argument_info_t<T>; // no need for remove_cvref, we do that before calling process_argument
+
+        std::string main_command_or_subcommand;
+
+        if(subcommand.name == subcommand_name_t{"MAIN"}) {
+            main_command_or_subcommand = "main command";
+        }
+        else {
+            main_command_or_subcommand = "subcommand";
+        }
+
+        if constexpr(arg_info_t::is_raw_type) {
+            return process_raw_type<T, last>(cumulative_positional_index, subcommand,
+                                                           main_command_or_subcommand);
+        }
+        else {
+            return process_wrapper<T, last>(subcommand, main_command_or_subcommand);
+        }
+    }
+}
